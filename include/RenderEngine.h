2ea351f9cb30e648264dd519f9ca5810e3ce3ab4
Brock York <twunknown[put at here]gmail.com>
2014-07-21 19:55:06 +0800

    Added cleanup function declaration

--- include/RenderEngine.h
+++ include/RenderEngine.h
@@ -14,6 +14,7 @@ class RenderEngine
         //void draw(SDL_Texture sprite, SDL_Rect *src, SDL_Rect *dest);
         void show();
         void setWindowTitle(std::string title);
+        bool cleanup();
     protected:
     private:
         SDL_Renderer *renderer;
