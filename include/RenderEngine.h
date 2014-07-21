04e2395940be60fc258df046abd3dbb0586681cd
Brock York <twunknown[put at here]gmail.com>
2014-07-21 14:45:29 +0800

    Add variables to hold screen dimensions from BallGame class and new function declaration for setting the window title

--- include/RenderEngine.h
+++ include/RenderEngine.h
@@ -2,6 +2,7 @@
 #define RENDERENGINE_H
 
 #include <SDL2/SDL.h>
+#include <iostream>
 
 class RenderEngine
 {
@@ -12,10 +13,13 @@ class RenderEngine
         void clearScreen();
         //void draw(SDL_Texture sprite, SDL_Rect *src, SDL_Rect *dest);
         void show();
+        void setWindowTitle(std::string title);
     protected:
     private:
         SDL_Renderer *renderer;
         SDL_Window *mainWindow;
+        int screenWidth;
+        int screenHeight;
 };
 
 #endif // RENDERENGINE_H
