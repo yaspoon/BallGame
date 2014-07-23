34049b0d3ad9a4d3c371c875b5cd41f380bb55af
Brock York <twunknown[put at here]gmail.com>
2014-07-23 20:37:07 +0800

    Well the fact i've implemented the entire class since last commiting means a lot has changed... Added getters and setters for all data filled out the constructor to set everything to sane defaults

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -1,11 +1,76 @@
 #include "Resource.h"
+#include <string.h>
 
 Resource::Resource()
 {
-    //ctor
+    id = -1;
+    references = 0;
+    type = RES_UNKNOWN;
+    resourcePath = "";
+    memset(&resource, 0, sizeof(resource));
 }
 
 Resource::~Resource()
 {
     //dtor
+}
+
+int Resource::getId()
+{
+    return id;
+}
+
+std::string Resource::getResourcePath()
+{
+    return resourcePath;
+}
+
+ResourceType Resource::getType()
+{
+    return type;
+}
+
+SDL_Surface *Resource::getSurface()
+{
+    return resource.surface;
+}
+
+SDL_Texture *Resource::getTexture()
+{
+    return resource.texture;
+}
+
+int Resource::getReferenceCount()
+{
+    return references;
+}
+
+void Resource::setId(int newId)
+{
+    id = newId;
+}
+
+void Resource::setType(ResourceType newType)
+{
+    type = newType;
+}
+
+void Resource::setSurface(SDL_Surface *newSurface)
+{
+    resource.surface = newSurface;
+}
+
+void Resource::setTexture(SDL_Texture *newTexture)
+{
+    resource.texture = newTexture;
+}
+
+void Resource::setReferenceCount(int refCount)
+{
+    references = refCount;
+}
+
+void Resource::setResourcePath(std::string filepath)
+{
+    resourcePath = filepath;
 }
