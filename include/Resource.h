4b118137c86aa11460544d51371da3bafaf8c547
Brock York <twunknown[put at here]gmail.com>
2014-07-23 20:33:46 +0800

    Added all the function declarations for the Resource object Added other data a resource object needs Renamed count because it didnt' really get accross the meaning in that it is used to reference count the resource so it can be cleaned up if needed

--- include/Resource.h
+++ include/Resource.h
@@ -1,6 +1,9 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
+#include <SDL2/SDL.h>
+#include <string>
+
 enum ResourceType
 {
         RES_UNKNOWN = 0,
@@ -14,11 +17,29 @@ class Resource
     public:
         Resource();
         virtual ~Resource();
+        int getId();
+        std::string getResourcePath();
+        ResourceType getType();
+        SDL_Surface *getSurface();
+        SDL_Texture *getTexture();
+        int getReferenceCount();
+        void setId(int newId);
+        void setType(ResourceType newType);
+        void setSurface(SDL_Surface *newSurface);
+        void setTexture(SDL_Texture *newTexture);
+        void setReferenceCount(int refCount);
+        void setResourcePath(std::string filepath);
     protected:
     private:
         int id;
-        int count; //Number of objects holding a reference to this object
+        int references; //Number of objects holding a reference to this object
         ResourceType type;
+        std::string resourcePath;
+        union
+        {
+            SDL_Surface *surface;
+            SDL_Texture *texture;
+        } resource;
 };
 
 #endif // RESOURCE_H
