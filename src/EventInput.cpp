236e67628bf406ff20eb1ad0149ae6f64346860e
Brock York <twunknown[put at here]gmail.com>
2014-07-28 20:25:23 +0800

    Default constructor sets type to unknown event sicne it should never be hit Alt constructor just inits variables with given data Can get the type and get the key that was pressed Haven't implemented any controller stuff #yolo

--- src/EventInput.cpp
+++ src/EventInput.cpp
@@ -2,10 +2,32 @@
 
 EventInput::EventInput()
 {
-    //ctor
+    inputType = EV_INPUT_UNKNOWN;
+}
+
+EventInput::EventInput(EventInputType newType, InputKey newKey)
+{
+    inputType = newType;
+    key = newKey;
+}
+
+EventInput::EventInput(EventInputType newType, InputController newController)
+{
+    inputType = newType;
+    controller = newController;
 }
 
 EventInput::~EventInput()
 {
     //dtor
+}
+
+EventInputType EventInput::getType()
+{
+    return inputType;
+}
+
+InputKey EventInput::getKey()
+{
+    return key;
 }
