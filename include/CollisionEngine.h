c6828f3fb45b1bfc5de62997e07a953f8c54160d
Brock York <twunknown[put at here]gmail.com>
2014-07-28 21:53:34 +0800

    Collision engine has a function to take in a list of collidable entities and then process them for collisions

--- include/CollisionEngine.h
+++ include/CollisionEngine.h
@@ -1,12 +1,15 @@
 #ifndef COLLISIONENGINE_H
 #define COLLISIONENGINE_H
 
+#include "CollisionEntity.h"
+#include "EventEngine.h"
 
 class CollisionEngine
 {
     public:
         CollisionEngine();
         virtual ~CollisionEngine();
+        void handleCollisions(std::vector<CollisionEntity*> collidables, std::shared_ptr<EventEngine> eventEngine);
     protected:
     private:
 };
--- src/CollisionEngine.cpp
+++ src/CollisionEngine.cpp
@@ -9,3 +9,8 @@ CollisionEngine::~CollisionEngine()
 {
     //dtor
 }
+
+void CollisionEngine::handleCollisions(std::vector<CollisionEntity*> collidables, std::shared_ptr<EventEngine> eventEngine)
+{
+    STUB("Collision handling not implemented yet");
+}
