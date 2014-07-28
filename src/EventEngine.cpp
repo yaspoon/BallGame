a28f07f3a824d938fa2095928f99d7acc32252f7
Brock York <twunknown[put at here]gmail.com>
2014-07-28 20:20:51 +0800

    Class implementation to handle and generate events Generate events gets events from SDL and then converts them into game events that classes would want to know about Key inputs are mapped so that a key type can change on the fly and still generate the correct event This is done using my own weird if-switch statement because switchs' need constant types to check against I hope Robey doesn't hate me for it. Entities can register and deregister for events ProcessEvents will generateEvents and then distribute them to objects that wanted them

--- src/EventEngine.cpp
+++ src/EventEngine.cpp
@@ -1,9 +1,13 @@
 #include "EventEngine.h"
 #include "SDL2/SDL.h"
+#include "boost/foreach.hpp"
 
 EventEngine::EventEngine()
 {
-    //ctor
+    inputMap[KEY_LEFT] = SDL_SCANCODE_LEFT;
+    inputMap[KEY_RIGHT] = SDL_SCANCODE_RIGHT;
+    inputMap[KEY_JUMP] = SDL_SCANCODE_SPACE;
+    systemMap[EV_SYS_QUIT] = SDL_SCANCODE_Q;
 }
 
 EventEngine::~EventEngine()
@@ -11,7 +15,7 @@ EventEngine::~EventEngine()
     //dtor
 }
 
-void EventEngine::processEvents()
+void EventEngine::generateEvents()
 {
     SDL_Event event;
     while( SDL_PollEvent(&event))
@@ -20,19 +24,162 @@ void EventEngine::processEvents()
         {
             case SDL_QUIT:
             {
-                EventSystem sysEv(EV_SYS_QUIT);
-                Event tmp(sysEv);
-                events.push_back(tmp);
+                systemEvents.push_back(EventSystem(EV_SYS_QUIT));
             }
                 break;
             case SDL_KEYDOWN:
+                {
+                    if(event.key.repeat == 0)
+                    {
+                        if(systemMap[EV_SYS_QUIT] == event.key.keysym.scancode)
+                        {
+                            systemEvents.push_back(EventSystem(EV_SYS_QUIT));
+                            break;
+                        }
 
+                        if(inputMap[KEY_LEFT] == event.key.keysym.scancode)
+                        {
+                            inputEvents.push_back(EventInput(EV_INPUT_KEYDOWN, KEY_LEFT));
+                            break;
+                        }
+
+                        if(inputMap[KEY_RIGHT] == event.key.keysym.scancode)
+                        {
+                            inputEvents.push_back(EventInput(EV_INPUT_KEYDOWN, KEY_RIGHT));
+                            break;
+                        }
+
+                        if(inputMap[KEY_JUMP] == event.key.keysym.scancode)
+                        {
+                            inputEvents.push_back(EventInput(EV_INPUT_KEYDOWN, KEY_JUMP));
+                            break;
+                        }
+                        /*switch(event.key.keysym.scancode)
+                        {
+                            case SDL_SCANCODE_Q:
+                                systemEvents.push_back(EventSystem(EV_SYS_QUIT));
+                            break;
+                            case SDL_SCANCODE_LEFT:
+                                inputEvents.push_back(EventInput(EV_INPUT_KEYDOWN, KEY_LEFT));
+                            break;
+                            case SDL_SCANCODE_RIGHT:
+                                inputEvents.push_back(EventInput(EV_INPUT_KEYDOWN, KEY_RIGHT));
+                            break;
+                        }*/
+                    }
+                }
                 break;
             case SDL_KEYUP:
+                {
+                    if(event.key.repeat == 0)
+                    {
+                        if(inputMap[KEY_LEFT] == event.key.keysym.scancode)
+                        {
+                            inputEvents.push_back(EventInput(EV_INPUT_KEYUP, KEY_LEFT));
+                            break;
+                        }
 
+                        if(inputMap[KEY_RIGHT] == event.key.keysym.scancode)
+                        {
+                            inputEvents.push_back(EventInput(EV_INPUT_KEYUP, KEY_RIGHT));
+                            break;
+                        }
+                        /*switch(event.key.keysym.scancode)
+                        {
+                            case SDL_SCANCODE_LEFT:
+                                inputEvents.push_back(EventInput(EV_INPUT_KEYUP, KEY_LEFT));
+                            break;
+                            case SDL_SCANCODE_RIGHT:
+                                inputEvents.push_back(EventInput(EV_INPUT_KEYUP, KEY_RIGHT));
+                            break;
+                        }*/
+                    }
+                }
                 break;
             default:
                 break;
         }//End switch( events.type )
     }//End event polling loop
 }
+
+
+void EventEngine::processEvents()
+{
+    generateEvents();
+
+    BOOST_FOREACH(EventSystem sysEv, systemEvents)
+    {
+        BOOST_FOREACH(Entity *entity, systemEventEntities)
+        {
+            entity->handleSystemEvent(sysEv);
+        }
+    }
+
+    BOOST_FOREACH(EventInput inputEv, inputEvents)
+    {
+        BOOST_FOREACH(Entity *entity, inputEventEntities)
+        {
+            entity->handleInputEvent(inputEv);
+        }
+    }
+
+    BOOST_FOREACH(EventCollision collEv, collisionEvents)
+    {
+        BOOST_FOREACH(CollisionEntity *entity, collisionEventEntities)
+        {
+            entity->handleCollisionEvent(collEv.getCollider());
+        }
+    }
+
+    inputEvents.clear();
+    collisionEvents.clear();
+
+}
+
+std::vector<EventSystem> EventEngine::getSystemEvents()
+{
+    return systemEvents;
+}
+
+bool EventEngine::registerForEvents(Entity *entityToReg, EventType typeToReg)
+{
+    bool retval = false;
+    switch(typeToReg)
+    {
+        case EV_SYSTEM:
+            systemEventEntities.push_back(entityToReg);
+        break;
+        case EV_INPUT:
+            inputEventEntities.push_back(entityToReg);
+        break;
+        case EV_COLLISION:
+            collisionEventEntities.push_back((CollisionEntity*)entityToReg);
+        break;
+        default:
+        break;
+    }
+
+    return retval;
+}
+
+ bool EventEngine::deregisterForEvents(Entity *entityToDeReg, EventType typeToDeReg)
+ {
+     switch(typeToDeReg)
+     {
+        case EV_SYSTEM:
+        break;
+        case EV_INPUT:
+        {
+            for(int i = 0; i < inputEventEntities.size(); i++)
+            {
+                if(inputEventEntities[i] == entityToDeReg)
+                {
+                    inputEventEntities.erase(inputEventEntities.begin() + i);
+                }
+            }
+        }
+        break;
+        case EV_COLLISION:
+        break;
+     }
+ }
