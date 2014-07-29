59f1a2bd298a1aae4a8fec76cf588f3ffbb686f7
Brock York <twunknown[put at here]gmail.com>
2014-07-29 21:55:15 +0800

    Moved Collidable type from visualentity and onground variable from player into class definition Added functions to access and update those new variable

--- include/CollisionEntity.h
+++ include/CollisionEntity.h
@@ -11,13 +11,19 @@ class CollisionEntity: public VisualEntity
         virtual ~CollisionEntity();
         float getXvel();
         float getYvel();
+        bool getOnGround();
+        COLLIDABLE getCtype();
         void setXvel(float newVel);
         void setYvel(float newVel);
+        void setOnGround(bool isOnGround);
+        void setCtype(COLLIDABLE type);
         virtual void handleCollisionEvent(CollisionEntity CollisionEntity) {}
     protected:
     private:
         float xvel;
         float yvel;
+        bool onGround;
+        COLLIDABLE cType;
 };
 
 #endif // COLLISIONENTITY_H
