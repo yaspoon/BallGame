ec99ea886194de1d8ab59359020cb61496f76945
Brock York <twunknown[put at here]gmail.com>
2014-07-30 23:37:24 +0800

    Removed old stale "collision" function Implemented funtions force onto us by being a decendant of entity

--- src/Block.cpp
+++ src/Block.cpp
@@ -1,37 +1,56 @@
 #include "Block.h"
 
 //--------------------------------------Constructors/
-    Block::Block()
-    :CollisionEntity( "./images/block.bmp", 20.0f, 20.0f, 20.0f, 20.0f )
-    {
-        CollisionEntity::setCtype( C_IMMOVABLE);
+Block::Block()
+:CollisionEntity("./images/block.bmp", 20.0f, 20.0f, 20.0f, 20.0f )
+{
+    CollisionEntity::setCtype( C_IMMOVABLE);
 
-    }
+}
 
-    Block::Block( int flags, int width, int height, int bpp, float x, float y )
-    :CollisionEntity( "./images/block.bmp", x, y, width, height )
-    {
-        CollisionEntity::setCtype( C_IMMOVABLE );
-    }
+Block::Block(int width, int height, float x, float y )
+:CollisionEntity( "./images/block.bmp", x, y, width, height )
+{
+    CollisionEntity::setCtype( C_IMMOVABLE );
+}
 
 //--------------------------------------Destructors/
-    Block::~Block()
+Block::~Block()
+{
+
+}
+
+void Block::handleInputEvent(EventInput input)
+{
+    switch(input.getType())
     {
+        case EV_INPUT_KEYDOWN:
+        case EV_INPUT_KEYUP:
+        case EV_INPUT_UNKNOWN:
+        default:
+            break;
 
     }
+}
 
-//--------------------------------------Methods/
-    //------------------------------Accessors
-
-    //------------------------------Mutators
-
-    //------------------------------Imperita
-    void Block::update( float dt )
+void Block::handleSystemEvent(EventSystem system)
+{
+    switch(system.getType())
     {
-
+        case EV_SYS_QUIT:
+        case EV_SYS_UNKNOWN:
+        default:
+            break;
     }
+}
 
-    void Block::collision( VisualEntity& collider )
-    {
+void Block::handleCollisionEvent(CollisionEntity *collision)
+{
+    collision = NULL;
+}
 
-    }
+CollisionEntity *Block::clone()
+{
+    Block *tmp = new Block(*this);
+    return (CollisionEntity*)tmp;
+}
