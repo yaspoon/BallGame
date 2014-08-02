474560fbf1e92e4a039aa9baf64f862640b28b9b
Brock York <twunknown@gmail.com>
2014-08-02 19:24:36 +0800

    Added support functions to implement collision handling

--- include/CollisionEngine.h
+++ include/CollisionEngine.h
@@ -20,9 +20,14 @@ class CollisionEngine
         virtual ~CollisionEngine();
         void handleCollisions(std::vector<CollisionEntity*> collidables, std::shared_ptr<EventEngine> eventEngine);
         CollisionResult testForCollision(CollisionEntity* collider1, CollisionEntity *collider2);
-        CollisionResult checkForOverlap(std::vector<float> collider1Points, std::vector<float> collider2Points);
     protected:
     private:
+        CollisionResult checkForOverlap(std::pair<float,float> collider1Points, std::pair<float,float> collider2Points);
+        bool isInside(float point, std::pair<float,float> points);
+        bool isContained(std::pair<float,float> collider1Points, std::pair<float,float> collider2Points);
+        float distanceBetween(float firstPoint, float secondPoint);
+        bool isColliding(Rect collider1, Rect collider2);
+        bool isInside2(vec2 point, Rect rectangle);
 };
 
 #endif // COLLISIONENGINE_H
