86cd3e235e18fc2effbbfb8d7b47483ee287b45d
Brock York <twunknown[put at here]gmail.com>
2014-08-06 19:18:33 +0800

    Moved collision handling functions into CollisionEntity Also cleaned up declarations so they are grouped by what they do constructors, getters etc

--- include/VisualEntity.h
+++ include/VisualEntity.h
@@ -22,11 +22,8 @@ class VisualEntity: public Entity
 {
     public:
         VisualEntity();
-
         VisualEntity( std::string filename, float x_i, float y_i, float width, float height, LAYER drawingLayer);
-
         VisualEntity(const VisualEntity& vEntity); //Copy constructor, needed so that reference counting in resource manager works...
-
         virtual ~VisualEntity();
 
         virtual void draw(RenderEngine renderEngine, vec2 offset);
@@ -36,20 +33,6 @@ class VisualEntity: public Entity
         void setPos( float x, float y );
 
         void setPrevPos( float x, float y );
-
-        float dotProduct( vec2 one, vec2 two );
-
-        std::vector<float> projectOnto( vec2 axis );
-
-        std::pair<float,float> minMaxProjectOnto(vec2 axis);
-
-        std::vector<vec2> getVertices();
-
-        std::vector<vec2> getAxes();
-
-        std::vector<vec2> getPerpAxes();
-
-        std::vector<vec2> getNormedAxes();
 
         int getSprite() {return sprite;}
 
