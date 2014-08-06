c1852b0d9c261679022e005695baa24154c50897
Brock York <twunknown[put at here]gmail.com>
2014-08-06 19:19:19 +0800

    Moved collision handling functions from VisualEntity into CollisionEntity

--- include/CollisionEntity.h
+++ include/CollisionEntity.h
@@ -25,10 +25,12 @@ class CollisionEntity: public VisualEntity
         CollisionEntity(std::string filename, float x, float y, float width, float height, LAYER drawingLayer, ColliderShapeType shapeType);
         CollisionEntity(std::string filename, float x, float y, float width, float height, LAYER drawingLayer, ColliderShapeType shapeType, CollisionShape shape);
         virtual ~CollisionEntity();
+
         float getXvel();
         float getYvel();
         bool getOnGround();
         COLLIDABLE getCtype();
+
         void setXvel(float newVel);
         void setYvel(float newVel);
         void setOnGround(bool isOnGround);
@@ -36,6 +38,15 @@ class CollisionEntity: public VisualEntity
         void setCollisionShape(CollisionShape newShape);
         virtual void handleCollisionEvent(CollisionEntity *CollisionEntity, CollisionResult result) =0;
         virtual CollisionEntity *clone() =0;
+
+        float dotProduct( vec2 one, vec2 two );
+        std::vector<float> projectOnto( vec2 axis );
+        std::pair<float,float> minMaxProjectOnto(vec2 axis);
+        std::vector<vec2> getVertices();
+        std::vector<vec2> getAxes();
+        std::vector<vec2> getPerpAxes();
+        std::vector<vec2> getNormedAxes();
+
     protected:
     private:
         float xvel;
--- src/CollisionEntity.cpp
+++ src/CollisionEntity.cpp
@@ -96,3 +96,159 @@ void CollisionEntity::setCollisionShape(CollisionShape newShape)
 {
     shape = newShape;
 }
+
+
+/*
+Creates the vertices of the box in the order
+top left
+topright
+bottom right
+bottom left
+
+since in sdl (0,0) is the top left of the window
+so why not start in the top left then
+*/
+std::vector<vec2> CollisionEntity::getVertices()
+{
+    std::vector<vec2> vertices;
+    Rect posDim = getPosDim();
+
+    vec2 topLeft;
+    topLeft.x = posDim.x;
+    topLeft.y = posDim.y;
+    vertices.push_back(topLeft);
+
+    vec2 topRight;
+    topRight.x = posDim.x + posDim.w;
+    topRight.y = posDim.y;
+    vertices.push_back(topRight);
+
+    vec2 bottomRight;
+    bottomRight.x = posDim.x + posDim.w;
+    bottomRight.y = posDim.y + posDim.h;
+    vertices.push_back(bottomRight);
+
+    vec2 bottomLeft;
+    bottomLeft.x = posDim.x;
+    bottomLeft.y = posDim.y + posDim.h;
+    vertices.push_back(bottomLeft);
+
+    return vertices;
+}
+
+std::vector<vec2> CollisionEntity::getAxes()
+{
+    std::vector<vec2> vertices = getVertices();
+    std::vector<vec2> axes;
+
+    for(std::vector<vec2>::iterator it = vertices.begin(); it != vertices.end(); ++it)
+    {
+        vec2 first = *it;
+        vec2 second;
+
+        std::vector<vec2>::iterator itDup = it;
+
+        if(++itDup == vertices.end())
+        {
+            second = *(vertices.begin());
+        }
+        else
+        {
+            second = *itDup;
+        }
+
+        vec2 axis;
+        axis.x = second.x - first.x;
+        axis.y = second.y - first.y;
+
+        axes.push_back(axis);
+
+    }
+
+    return axes;
+}
+
+std::vector<vec2> CollisionEntity::getPerpAxes()
+{
+    std::vector<vec2> axes = getAxes();
+
+    for(std::vector<vec2>::iterator it = axes.begin(); it != axes.end(); ++it)
+    {
+        vec2 axis = *it;
+
+        float tmp = axis.x;
+        axis.x = -(axis.y);
+        axis.y = tmp;
+
+        *it = axis;
+    }
+
+    return axes;
+}
+
+std::vector<vec2> CollisionEntity::getNormedAxes()
+{
+    /*std::vector<vec2> axes = getPerpAxes();
+
+    for(std::vector<vec2>::iterator it = axes.begin(); it != axes.end(); ++it)
+    {
+        vec2 axis = *it;
+
+        float length = sqrt((axis.x * axis.x) + (axis.y * axis.y));
+
+        axis.x = axis.x / length;
+        axis.y = axis.y / length;
+
+        *it = axis;
+    }*/
+
+    std::vector<vec2> axes;
+    vec2 xaxis = {1, 0};
+    vec2 yaxis = {0, 1};
+    axes.push_back(xaxis);
+    axes.push_back(yaxis);
+
+    return axes;
+}
+
+std::vector<float> CollisionEntity::projectOnto( vec2 axis )
+{
+    std::vector<float> projections;
+    std::vector<vec2> vertices = getVertices();
+
+    for(std::vector<vec2>::iterator it = vertices.begin(); it != vertices.end(); ++it) //project each vertex onto the given axis
+    {
+        vec2 vertex = *it;
+        float dp = dotProduct(vertex, axis);//vertex.x * axis.x) + (vertex.y * axis.y); //Calc dot product of vertex and axis to project it onto the axis
+        projections.push_back(dp);
+    }
+
+    return projections;
+}
+
+std::pair<float,float> CollisionEntity::minMaxProjectOnto(vec2 axis)
+{
+    std::vector<float> projectedPoints = projectOnto(axis);
+    float min = projectedPoints[0];
+    float max = projectedPoints[0];
+    for(std::vector<float>::iterator it = projectedPoints.begin(); it != projectedPoints.end(); ++it)
+    {
+        if(*it < min)
+        {
+            min = *it;
+        }
+        if(*it > max)
+        {
+            max = *it;
+        }
+    }
+    std::pair<float,float> retval;
+    retval.first = min;
+    retval.second = max;
+    return retval;
+}
+
+float CollisionEntity::dotProduct( vec2 one, vec2 two )
+{
+    return (one.x * two.x) + (one.y * two.y );
+}
